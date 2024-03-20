2310ce459e89e6a808949d4ae38e900c1f27a2b7
LeonardoViotti <leonardoviotti@gmail.com>
2024-03-20 12:59:56 -0400

    Solved conflicts now?

--- aldi/datasets.py
+++ aldi/datasets.py
@@ -22,5 +22,5 @@ register_coco_instances("cfc_channel_train", {},         "datasets/cfc/coco_labe
 register_coco_instances("cfc_channel_test",   {},         "datasets/cfc/coco_labels/cfc_channel_test.json",                    "datasets/cfc/images/cfc_channel_test/")
 
 # XC
-register_coco_instances("xc_train", {}, "datasets/xc/annotations/xc_train.json", "datasets/xc/images/train/")
-register_coco_instances("xc_val", {}, "datasets/xc/annotations/xc_val.json", "datasets/xc/images/val/")
+register_coco_instances("xc_train", {}, "datasets/xc/annotations/xc-train.json", "datasets/xc/images/train/")
+register_coco_instances("xc_val", {}, "datasets/xc/annotations/xc-val.json", "datasets/xc/images/val/")
